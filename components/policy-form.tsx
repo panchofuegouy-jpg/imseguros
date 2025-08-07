f9a2e428b176f9102e118e1a40ca9e1a6c0a9c32
/panchofuegouy-jpg <pancho.fuego.uy@gmail.com>
2025-08-07 15:59:08 -0300

    asd

--- components/policy-form.tsx
+++ components/policy-form.tsx
@@ -389,6 +389,18 @@ export default function PolicyForm({ clients, companies, onSubmit, initialData }
                         <SelectItem value="Hogar">Hogar</SelectItem>
                         <SelectItem value="Salud">Salud</SelectItem>
                         <SelectItem value="Empresarial">Empresarial</SelectItem>
+                        <SelectItem value="Empresarial">Otro</SelectItem>
+                        <SelectItem value="Empresarial">Camiones</SelectItem>
+                        <SelectItem value="Empresarial">Taxi</SelectItem>
+                        <SelectItem value="Empresarial">Agricola</SelectItem>
+                        <SelectItem value="Empresarial">Empresarial</SelectItem>
+                        <SelectItem value="Empresarial">Motos</SelectItem>
+                        
+
+
+
+
+
                       </SelectContent>
                     </Select>
                   </div>
